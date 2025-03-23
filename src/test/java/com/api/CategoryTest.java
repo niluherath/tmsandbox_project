8fbc96d8df25382d0bfb3da19a2942e995b1a12f
nilu herath <niluherath@hotmail.com>
2025-03-24 10:26:46 +1300

    refactored validateGalleryPromotionsText to iterate through the arraylist of promotion objects

--- src/test/java/com/api/CategoryTest.java
+++ src/test/java/com/api/CategoryTest.java
@@ -5,44 +5,66 @@ import com.pojo.Promotion;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import com.utils.ServiceUtils;
-
-import java.util.ArrayList;
-
+import org.testng.annotations.Test;
 
 public class CategoryTest {
 
-    private final int catId = 6327;
+    private final int CAT_ID = 6327;
     private final String CATEGORY_ID = "6327";
+    private final String CATEGORY_NAME = "Carbon credits";
+    private final String PROMOTION_NAME = "Gallery";
+    private final String GALLERY_DESCRIPTION = "Good position in category";
 
-    @org.testng.annotations.Test(description = "")
+    @Test(description = "This test validates that category id returned is 6327")
     public void validateCategoryId() {
 
         Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
         Assert.assertEquals(response.getStatusCode(), 200);
         System.out.println(response.asString());
         Category category = response.as(Category.class);
-        Assert.assertEquals(category.getCategoryId(), catId);
+        Assert.assertEquals(category.getCategoryId(), CAT_ID);
     }
 
-    @org.testng.annotations.Test(description = "")
+    @Test(description = "This test validates that category name for category id 6327 is 'Carbon credits'")
+    public void validateCategoryName() {
+
+        Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println(response.asString());
+        Category category = response.as(Category.class);
+        Assert.assertEquals(category.getName(), CATEGORY_NAME);
+    }
+
+    @Test(description = "This test validates that for category id 6327 canRelist is true")
     public void validateCanReList() {
 
         Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
         Assert.assertEquals(response.getStatusCode(), 200);
         System.out.println(response.asString());
         Category category = response.as(Category.class);
-        Assert.assertEquals(category.canRelist, true);
+        Assert.assertTrue(category.canRelist);
     }
 
-    @org.testng.annotations.Test(description = "")
+    @Test(description = "This test finds the promotion where name is 'Gallery' and validates that it's description is 'Good position in category'")
     public void validateGalleryPromotionsText() {
 
         Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
         Assert.assertEquals(response.getStatusCode(), 200);
         System.out.println(response.asString());
         Category category = response.as(Category.class);
-        ArrayList<Promotion> promotions = category.getPromotions();
-        Assert.assertEquals("Gallery", promotions.get(1).getName());
-        Assert.assertEquals("Good position in category", promotions.get(1).getDescription());
+        Assert.assertTrue(!category.getPromotions().isEmpty());
+
+        boolean isPromotionFound = false;
+
+        for (Promotion promotion : category.getPromotions()) {
+            if (promotion.getName().equals(PROMOTION_NAME)) {
+                Assert.assertEquals(promotion.getDescription(), GALLERY_DESCRIPTION);
+                isPromotionFound = true;
+                break;
+            }
+        }
+        if (!isPromotionFound) {
+            Assert.fail();
+        }
     }
 }
--- src/test/java/com/utils/ServiceUtils.java
+++ src/test/java/com/utils/ServiceUtils.java
@@ -1,19 +1,21 @@
 package com.utils;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServiceUtils {
 
     private final static String ACCEPT = "accept";
     private final static String ACCEPT_ENCODING = "accept-encoding";
-    private final static String ACCEPT_LANGUAGE = "accept-language";
-    private final static String ACCESS_CONTROL_REQUEST_HEADERS = "access-control-request-headers";
     private final static String ACCEPT_HEADERS = "*/*";
     private final static String ACCEPT_ENCODING_HEADERS = "gzip, deflate, br";
-    private final static String ACCEPT_LANGUAGE_HEADERS = "en-US,en;q=0.9";
-    private final static String ACCESS_CONTROL_REQUEST_HEADERS_VALUES = "authorization,newrelic,x-trademe-uniqueclientid";
+
 
     public static Response getCategoryResponse(String categoryId){
 
@@ -21,8 +23,6 @@ public class ServiceUtils {
         RequestSpecification request = RestAssured.given().relaxedHTTPSValidation();
         request.header(ACCEPT, ACCEPT_HEADERS);
         request.header(ACCEPT_ENCODING, ACCEPT_ENCODING_HEADERS);
-        request.header(ACCEPT_LANGUAGE, ACCEPT_LANGUAGE_HEADERS);
-        request.header(ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_REQUEST_HEADERS_VALUES);
         request.param("catalogue", false);
         return request.get("v1/Categories/" + categoryId + "/Details.json");
 
