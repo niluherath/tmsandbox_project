e766d9f68f2a00e113f69e3e2b46aeb4cc8218f7
nilu herath <niluherath@hotmail.com>
2025-03-23 17:42:26 +1300

    updating tests to pass the id to service util

--- src/test/java/com/api/CategoryTest.java
+++ src/test/java/com/api/CategoryTest.java
@@ -1,22 +1,48 @@
 package com.api;
 
 import com.pojo.Category;
+import com.pojo.Promotion;
 import io.restassured.response.Response;
 import org.testng.Assert;
-import utils.ServiceUtils;
+import com.utils.ServiceUtils;
+
+import java.util.ArrayList;
 
 
 public class CategoryTest {
 
     private final int catId = 6327;
+    private final String CATEGORY_ID = "6327";
 
     @org.testng.annotations.Test(description = "")
-    public void getCategoryInfoTest() {
+    public void validateCategoryId() {
 
-        Response response = ServiceUtils.getCategoryResponse(catId);
+        Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
         Assert.assertEquals(response.getStatusCode(), 200);
         System.out.println(response.asString());
         Category category = response.as(Category.class);
         Assert.assertEquals(category.getCategoryId(), catId);
+    }
+
+    @org.testng.annotations.Test(description = "")
+    public void validateCanReList() {
+
+        Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println(response.asString());
+        Category category = response.as(Category.class);
+        Assert.assertEquals(category.canRelist, true);
+    }
+
+    @org.testng.annotations.Test(description = "")
+    public void validateGalleryPromotionsText() {
+
+        Response response = ServiceUtils.getCategoryResponse(CATEGORY_ID);
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println(response.asString());
+        Category category = response.as(Category.class);
+        ArrayList<Promotion> promotions = category.getPromotions();
+        Assert.assertEquals("Gallery", promotions.get(1).getName());
+        Assert.assertEquals("Good position in category", promotions.get(1).getDescription());
     }
 }
--- src/test/java/com/utils/ServiceUtils.java
+++ src/test/java/com/utils/ServiceUtils.java
@@ -1,4 +1,5 @@
-package utils;
+package com.utils;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -13,9 +14,8 @@ public class ServiceUtils {
     private final static String ACCEPT_ENCODING_HEADERS = "gzip, deflate, br";
     private final static String ACCEPT_LANGUAGE_HEADERS = "en-US,en;q=0.9";
     private final static String ACCESS_CONTROL_REQUEST_HEADERS_VALUES = "authorization,newrelic,x-trademe-uniqueclientid";
-    private final static String CATEGORY_ID = "6327";
 
-    public static Response getCategoryResponse(int catId){
+    public static Response getCategoryResponse(String categoryId){
 
         RestAssured.baseURI = "https://api.tmsandbox.co.nz";
         RequestSpecification request = RestAssured.given().relaxedHTTPSValidation();
@@ -24,7 +24,7 @@ public class ServiceUtils {
         request.header(ACCEPT_LANGUAGE, ACCEPT_LANGUAGE_HEADERS);
         request.header(ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_REQUEST_HEADERS_VALUES);
         request.param("catalogue", false);
-        return request.get("v1/Categories/" + CATEGORY_ID + "/Details.json");
+        return request.get("v1/Categories/" + categoryId + "/Details.json");
 
     }
 
